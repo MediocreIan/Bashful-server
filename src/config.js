bed74046039b6585268dcb7cbe45933e6c76aa7f
MediocreIan <idrews094@gmail.com>
2020-04-16 16:00:14 -0500

    db connection issue

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
-  DATABASE_URL: "postgresql://ian@localhost/bash_generator",
+  DATABASE_URL:
+    process.env.DATABASE_URL || "postgresql://ian@localhost/bash_generator",
   TEST_DATABASE_URL: "postgresql://ian@localhost/bash_generator",
 };
